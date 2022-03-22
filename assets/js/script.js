8b6fd14033e9fe30def2fc07e70a30128209f279
Luiz Medina <luizmedina87@gmail.com>
2022-03-21 20:54:25 -0400

    date display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log(dayjs())
+
+
+$(document).ready(function() {
+  var dateFormat = "dddd, MMMM D, YYYY h:mm A";
+  var dateNow = dayjs().format(dateFormat);
+  $("#currentDay").text(dateNow);
+});
